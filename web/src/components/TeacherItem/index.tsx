9ad209b47d0d7feced7a90df1df03c3a8b5ee7c7
Talles Alves <tallesventura@yahoo.com.br>
2020-08-06 09:16:39 -0300

    segundo dia

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,38 +1,59 @@
 import React from 'react';
 
-import './styles.css'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem() {
+import './styles.css'
+import api from '../../services/api';
+
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars3.githubusercontent.com/u/14003158?s=400&u=ac417e9d6f64f8ffc43847f52790069b69d58f2b&v=4"
-          alt="Talles Oliveira"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Talles Oliveira</strong>
-          <span>Programação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac massa quis justo placerat pulvinar. Ut at elit quis lacus
-        maximus vehicula et et mi. Nunc ultrices lacus a metus rhoncus gravida. In eu aliquam erat, sed volutpat elit. Aenean pharetra
-        tincidunt dictum. Praesent nec mollis diam. Fusce commodo auctor gravida. Proin tincidunt viverra fermentum. Duis condimentum a
-        ex feugiat egestas.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 100,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+
+        <a
+          target="_blank" 
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
